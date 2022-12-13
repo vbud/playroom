1208fb1bff54a124c893f9629d6502ed9f64f92d
Valjean Clark <valjeanbud@gmail.com>
2022-12-13 15:11:39 -0800

    mui Select snippet

---
+++ configs/mui/snippets/Select.snippets.tsx
@@ -0,0 +1,19 @@
+export const snippets = [
+  {
+    name: 'basic',
+    code: `
+<FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={10}
+    label="Age"
+  >
+    <MenuItem value={10}>Ten</MenuItem>
+    <MenuItem value={20}>Twenty</MenuItem>
+    <MenuItem value={30}>Thirty</MenuItem>
+  </Select>
+</FormControl>`,
+  },
+];
