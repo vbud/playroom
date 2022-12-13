dbdbc0fee86095f603be797f0f26cea6aaf0fb8a
Valjean Clark <valjeanbud@gmail.com>
2022-12-13 15:11:16 -0800

    re-export all mui components

--- configs/mui/components.js
+++ configs/mui/components.js
@@ -1,1 +1,13 @@
-export { Alert, Button } from '@mui/material';
+const muiComponents = require('@mui/material');
+
+const components = {};
+
+Object.keys(muiComponents).forEach((name) => {
+  // Only export components, assuming exports that start with a capital letter
+  // are always components.
+  if (name[0].toUpperCase() === name[0]) {
+    components[name] = muiComponents[name];
+  }
+});
+
+module.exports = components;
