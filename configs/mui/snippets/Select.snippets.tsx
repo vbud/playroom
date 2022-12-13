c179df85444be475fa85acc46ab6cb28f78ec83e
Valjean Clark <valjeanbud@gmail.com>
2022-12-13 15:32:39 -0800

    simplify mui Select snippet

--- configs/mui/snippets/Select.snippets.tsx
+++ configs/mui/snippets/Select.snippets.tsx
@@ -3,13 +3,8 @@ export const snippets = [
     name: 'basic',
     code: `
 <FormControl fullWidth>
-  <InputLabel id="demo-simple-select-label">Age</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={10}
-    label="Age"
-  >
+  <InputLabel>Age</InputLabel>
+  <Select value={10} label="Age">
     <MenuItem value={10}>Ten</MenuItem>
     <MenuItem value={20}>Twenty</MenuItem>
     <MenuItem value={30}>Thirty</MenuItem>
