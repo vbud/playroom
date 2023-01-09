8de6ad21e0f60a3787d0a603b7b2696146ddb746
Valjean Clark <valjeanbud@gmail.com>
2023-01-09 15:15:05 -0800

    add workaround for flash of incorrectly positioned Space children

--- src/Playroom/Canvas/Canvas.tsx
+++ src/Playroom/Canvas/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { Space } from './ZoomableCanvas';
 import { useDebouncedCallback } from 'use-debounce';
 
@@ -23,10 +23,11 @@ export const Canvas = ({
   selectedFrameId,
 }: CanvasProps) => {
   const [{ canvasPosition }, dispatch] = useContext(StoreContext);
+  const [isViewportReady, setIsViewportReady] = useState(false);
 
   const canvasRef = useRef<HTMLDivElement>(null);
 
-  const onSpaceUpdate = useDebouncedCallback((vp) => {
+  const saveCanvasPosition = useDebouncedCallback((vp) => {
     dispatch({
       type: 'saveCanvasPosition',
       payload: { left: vp.left, top: vp.top, zoom: vp.zoomFactor },
@@ -49,18 +50,35 @@ export const Canvas = ({
           const { left, top, zoom } = canvasPosition;
           viewport.camera.updateTopLeft(left, top, zoom);
         }}
-        onUpdated={onSpaceUpdate}
+        onUpdated={(vp) => {
+          // Because viewport camera updates are asynchronous, Space's children render
+          // before the `onCreate` camera update containing the stored canvas position
+          // can finish. To avoid seeing a flash of incorrectly positioned children,
+          // we wait for the first call to `onUpdated`, after which we update a flag
+          // that tells us that the viewport has been updated, and we can safely
+          // render the children.
+          //
+          // The author is aware of the issue
+          // (https://github.com/aarondail/react-zoomable-ui/issues/49#issuecomment-1374714798)
+          // and may provide a fix in the future.
+          if (!isViewportReady) {
+            setIsViewportReady(true);
+          }
+
+          saveCanvasPosition(vp);
+        }}
       >
-        {Object.keys(frames).map((frameId) => (
-          <CanvasFrame
-            key={frameId}
-            frameConfig={frames[frameId]}
-            components={components}
-            selectedFrameId={selectedFrameId}
-            scale={canvasPosition.zoom}
-            canvasEl={canvasRef.current}
-          />
-        ))}
+        {isViewportReady &&
+          Object.keys(frames).map((frameId) => (
+            <CanvasFrame
+              key={frameId}
+              frameConfig={frames[frameId]}
+              components={components}
+              selectedFrameId={selectedFrameId}
+              scale={canvasPosition.zoom}
+              canvasEl={canvasRef.current}
+            />
+          ))}
       </Space>
     </div>
   );
